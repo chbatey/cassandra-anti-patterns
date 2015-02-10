d23b18b1d789e065e68f62592ca847ea7fff0cf0
Christopher Batey <christopher.batey@gmail.com>
2015-02-10 18:04:39 +0000

    example of a good logged batch

--- src/main/java/info/batey/examples/cassandra/CustomerEventDao.java
+++ src/main/java/info/batey/examples/cassandra/CustomerEventDao.java
@@ -11,28 +11,49 @@ public class CustomerEventDao implements Closeable {
 
     private final static String keyspace = "CREATE KEYSPACE IF NOT EXISTS events WITH replication = {'class': 'NetworkTopologyStrategy', 'datacenter1' : 3 }";
     private final static String eventsTable = "CREATE TABLE if NOT EXISTS customer_events ( customer_id text , statff_id text , store_type text, time timeuuid , event_type text , primary KEY (customer_id, time)) ";
-    private final static String insertEvent = "INSERT INTO events.customer_events (customer_id, time , event_type , statff_id , store_type ) VALUES ( ?, ?, ?, ?, ?)";
+    private final static String staffEventsTable = "CREATE TABLE if NOT EXISTS customer_events_by_staff_id ( customer_id text , statff_id text , store_type text, time timeuuid , event_type text , primary KEY (staff_id, time)) ";
+    private final static String insertByCustomerIdCql = "INSERT INTO events.customer_events (customer_id, time , event_type , statff_id , store_type ) VALUES ( ?, ?, ?, ?, ?)";
+    private final static String insertByStaffIdCql = "INSERT INTO events.customer_events_by_staff_id (customer_id, time , event_type , statff_id , store_type ) VALUES ( ?, ?, ?, ?, ?)";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomerEventDao.class);
 
-    private final Session session;
-    private final PreparedStatement insertStatement;
     private final Cluster cluster;
+    private final Session session;
+    private final PreparedStatement insertByCustomerId;
+    private final PreparedStatement insertByStaffId;
 
     public CustomerEventDao() {
         cluster = Cluster.builder().addContactPoint("localhost").build();
         session = cluster.connect("events");
         session.execute(keyspace);
         session.execute(eventsTable);
+        session.execute(staffEventsTable);
         session.execute("use events");
-        insertStatement = session.prepare(insertEvent);
+        insertByCustomerId = session.prepare(insertByCustomerIdCql);
+        insertByStaffId = session.prepare(insertByStaffIdCql);
     }
 
     public void storeEvent(ConsistencyLevel consistencyLevel, CustomerEvent customerEvent) {
-        BoundStatement boundInsert = insertStatement.bind(customerEvent.getCustomerId(), customerEvent.getTime(), customerEvent.getEventType(), customerEvent.getStaffId(), customerEvent.getStaffId());
+        BoundStatement boundInsert = insertByCustomerId.bind(customerEvent.getCustomerId(), customerEvent.getTime(), customerEvent.getEventType(), customerEvent.getStaffId(), customerEvent.getStaffId());
         boundInsert.enableTracing();
         boundInsert.setConsistencyLevel(consistencyLevel);
         ResultSet execute = session.execute(boundInsert);
+        logTraceInfo(execute.getExecutionInfo());
+    }
+
+
+    public void storeEventLogged(ConsistencyLevel consistencyLevel, CustomerEvent customerEvent) {
+        BoundStatement boundInsertForCustomerId = insertByCustomerId.bind(customerEvent.getCustomerId(), customerEvent.getTime(), customerEvent.getEventType(), customerEvent.getStaffId(), customerEvent.getStaffId());
+        BoundStatement boundInsertForStaffId= insertByStaffId.bind(customerEvent.getCustomerId(), customerEvent.getTime(), customerEvent.getEventType(), customerEvent.getStaffId(), customerEvent.getStaffId());
+
+        BatchStatement batchStatement = new BatchStatement(BatchStatement.Type.LOGGED);
+        batchStatement.enableTracing();
+        batchStatement.setConsistencyLevel(consistencyLevel);
+        batchStatement.add(boundInsertForCustomerId);
+        batchStatement.add(boundInsertForStaffId);
+
+
+        ResultSet execute = session.execute(batchStatement);
         logTraceInfo(execute.getExecutionInfo());
     }
 
@@ -53,7 +74,7 @@ public class CustomerEventDao implements Closeable {
         batchStatement.enableTracing();
 
         for (CustomerEvent event : events) {
-            BoundStatement boundInsert = insertStatement.bind(
+            BoundStatement boundInsert = insertByCustomerId.bind(
                     customerId,
                     event.getTime(),
                     event.getEventType(),
@@ -79,7 +100,7 @@ public class CustomerEventDao implements Closeable {
     }
 
     private BoundStatement createBoundStatement(ConsistencyLevel consistencyLevel, CustomerEvent customerEvent) {
-        BoundStatement boundInsert = insertStatement.bind(customerEvent.getCustomerId(), customerEvent.getTime(), customerEvent.getEventType(), customerEvent.getStaffId(), customerEvent.getStaffId());
+        BoundStatement boundInsert = insertByCustomerId.bind(customerEvent.getCustomerId(), customerEvent.getTime(), customerEvent.getEventType(), customerEvent.getStaffId(), customerEvent.getStaffId());
         boundInsert.enableTracing();
         boundInsert.setConsistencyLevel(consistencyLevel);
         return boundInsert;
